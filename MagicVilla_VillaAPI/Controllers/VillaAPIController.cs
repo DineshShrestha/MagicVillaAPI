0738b004c22fd01386dddaaab36a44c7305de04a
Dinesh <dinu.shrestha777@gmail.com>
2022-10-10 13:14:23 +0200

    added asNoTracking fuctionality to remove ambiguity in model id paramater

--- MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -4,6 +4,7 @@ using MagicVilla_VillaAPI.Models;
 using MagicVilla_VillaAPI.Models.Dto;
 using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace MagicVilla_VillaAPI.Controllers
 {   // [Route("api/[controller]")]
@@ -120,11 +121,11 @@ namespace MagicVilla_VillaAPI.Controllers
             {
                 return BadRequest();
             }
-            var villa = _db.Villas.FirstOrDefault(u => u.Id == id);
+            //var villa = _db.Villas.FirstOrDefault(u => u.Id == id);
             //villa.Name = villaDTO.Name;
             //villa.Sqft = villaDTO.Sqft;
             //villa.Occupancy = villaDTO.Occupancy;
-            Villa model = new()
+            Villa model = new Villa()
             {
                 Amenity = villaDTO.Amenity,
                 Details = villaDTO.Details,
@@ -150,7 +151,7 @@ namespace MagicVilla_VillaAPI.Controllers
             {
                 return BadRequest();
             }
-            var villa = _db.Villas.FirstOrDefault(u => u.Id == id);
+            var villa = _db.Villas.AsNoTracking().FirstOrDefault(u => u.Id == id);
 
             VillaDTO villaDTO = new()
             {
